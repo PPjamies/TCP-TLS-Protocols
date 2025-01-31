226f2e319b846d3888ffa04b780d9a8ea80f9787
PJ <pazuzujindrich@gmail.com>
2025-01-31 14:03:19 -0800

    implemented custom error handling in decoder. implemented logic for getters in decoder.

--- src/tls/tls_record_decoder.rs
+++ src/tls/tls_record_decoder.rs
@@ -1,7 +1,96 @@
-use crate::tls::tls_record::{ApplicationDataRecord, ChangeCipherSpecRecord, HandshakeFinishedRecord, HandshakeHeader, HelloRecord, KeyExchangeRecord, RecordHeader};
+use crate::tls::tls_record::{
+    ApplicationDataRecord, ChangeCipherSpecRecord, HandshakeFinishedRecord, HandshakeHeader,
+    HelloRecord, KeyExchangeRecord, RecordHeader,
+};
 
+use crate::tls::tls_constants::{
+    TLS_HANDSHAKE_CLIENT_HELLO, TLS_HANDSHAKE_CLIENT_KEY_EXCHANGE, TLS_HANDSHAKE_FINISHED,
+    TLS_HANDSHAKE_MESSAGE_LENGTH, TLS_PROTOCOL_VERSION, TLS_RECORD_ALERT,
+    TLS_RECORD_APPLICATION_DATA, TLS_RECORD_CHANGE_CIPHER_SPEC, TLS_RECORD_HANDSHAKE,
+    TLS_RSA_AES_128_CBC_SHA_256,
+};
 use nom::multi::count;
 use nom::{bytes::complete::take, IResult};
+use std::result::Result;
+
+#[derive(Debug)]
+pub enum DecoderError {
+    NomError(nom::Err<nom::error::Error<Vec<u8>>>),
+    RecordTypeNotFound,
+    HandshakeTypeNotFound,
+    InvalidClientHelloRecord,
+    InvalidClientKeyExchangeRecord,
+    InvalidClientChangeCipherSpecRecord,
+    InvalidClientHandshakeFinishedRecord,
+    InvalidClientAlertRecord,
+    InvalidClientApplicationDataRecord,
+    InvalidRecordType,
+    UnsupportedProtocolVersion,
+    InvalidHandshakeMessageLength,
+    InvalidHandshakeType,
+    UnsupportedCipherSuite,
+}
+impl std::fmt::Display for DecoderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            DecoderError::NomError(err) => write!(f, "Parsing error: {:?}", err),
+            DecoderError::RecordTypeNotFound => write!(f, "Record type not found"),
+            DecoderError::HandshakeTypeNotFound => write!(f, "Handshake type not found"),
+            DecoderError::InvalidClientHelloRecord => write!(f, "Invalid Client Hello Record"),
+            DecoderError::InvalidClientKeyExchangeRecord => {
+                write!(f, "Invalid Client Key Exchange Record")
+            }
+            DecoderError::InvalidClientChangeCipherSpecRecord => {
+                write!(f, "Invalid Client Change Cipher Spec Record")
+            }
+            DecoderError::InvalidClientHandshakeFinishedRecord => {
+                write!(f, "Invalid Client Handshake Finished Record")
+            }
+            DecoderError::InvalidClientAlertRecord => write!(f, "Invalid Client Alert Record"),
+            DecoderError::InvalidClientApplicationDataRecord => {
+                write!(f, "Invalid Client Application Data Record")
+            }
+            DecoderError::InvalidRecordType => {
+                write!(f, "Invalid Record Type")
+            }
+            DecoderError::UnsupportedProtocolVersion => write!(f, "Unsupported Protocol Version"),
+            DecoderError::InvalidHandshakeMessageLength => {
+                write!(f, "Invalid Handshake Message Length")
+            }
+            DecoderError::InvalidHandshakeType => {
+                write!(f, "Invalid Handshake Type")
+            }
+            DecoderError::UnsupportedCipherSuite => write!(f, "Unsupported Cipher Suite"),
+        }
+    }
+}
+impl std::error::Error for DecoderError {}
+
+fn validate_record_header(
+    record_header: &RecordHeader,
+    expected_record_type: &u8,
+) -> Result<(), DecoderError> {
+    if &record_header.record_type != expected_record_type {
+        return Err(DecoderError::InvalidRecordType);
+    }
+    if &record_header.protocol_version != &TLS_PROTOCOL_VERSION {
+        return Err(DecoderError::UnsupportedProtocolVersion);
+    }
+    if &record_header.handshake_message_length != &TLS_HANDSHAKE_MESSAGE_LENGTH {
+        return Err(DecoderError::InvalidHandshakeMessageLength);
+    }
+    Ok(())
+}
+
+fn validate_handshake_header(
+    handshake_header: &HandshakeHeader,
+    expected_handshake_type: &u8,
+) -> Result<(), DecoderError> {
+    if &handshake_header.handshake_type != expected_handshake_type {
+        return Err(DecoderError::InvalidHandshakeType);
+    }
+    Ok(())
+}
 
 fn decode_record_type(input: &[u8]) -> IResult<&[u8], u8> {
     let (input, record_type) = take(1usize)(input)?;
@@ -125,62 +214,99 @@ fn decode_handshake_finished_record(input: &[u8]) -> IResult<&[u8], HandshakeFin
     let (input, handshake_header) = decode_handshake_header(input)?;
     let (input, verify_data) = input.iter().collect();
 
-    Ok((input, HandshakeFinishedRecord { handshake_header, verify_data }))
+    Ok((
+        input,
+        HandshakeFinishedRecord {
+            handshake_header,
+            verify_data,
+        },
+    ))
 }
 
-//todo:
-pub fn get_record_type(input: &[u8]) -> IResult<&[u8], u8> {
+pub fn get_record_type(input: &[u8]) -> Result<u8, DecoderError> {
     decode_record_type(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::RecordTypeNotFound)
 }
 
-pub fn get_handshake_type(input: &[u8]) -> IResult<&[u8], u8> {
+pub fn get_handshake_type(input: &[u8]) -> Result<u8, DecoderError> {
     decode_handshake_type(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::HandshakeTypeNotFound)
 }
 
-//TODO:
-pub fn get_client_hello_record(input: &[u8]) -> HelloRecord {
-    let record = decode_hello_record(input)?;
+pub fn get_client_hello_record(input: &[u8]) -> Result<HelloRecord, DecoderError> {
+    let record = decode_hello_record(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::InvalidClientHelloRecord)?;
 
-    // validate record
+    validate_record_header(&record.record_header, &TLS_RECORD_HANDSHAKE)?;
+    validate_handshake_header(&record.handshake_header, &TLS_HANDSHAKE_CLIENT_HELLO)?;
+
+    let contains_supported_cipher_suite = &record
+        .cipher_suites
+        .iter()
+        .any(|cipher_suite| cipher_suite == &TLS_RSA_AES_128_CBC_SHA_256);
+
+    if !contains_supported_cipher_suite {
+        return Err(DecoderError::UnsupportedCipherSuite);
+    }
+    Ok(record)
 }
 
-pub fn get_client_key_exchange_record(input: &[u8]) -> KeyExchangeRecord {
-    let record = decode_key_exchange_record(input)?;
+pub fn get_client_key_exchange_record(input: &[u8]) -> Result<KeyExchangeRecord, DecoderError> {
+    let record = decode_key_exchange_record(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::InvalidClientKeyExchangeRecord)?;
+
+    validate_record_header(&record.record_header, &TLS_RECORD_HANDSHAKE)?;
+    validate_handshake_header(&record.handshake_header, &TLS_HANDSHAKE_CLIENT_KEY_EXCHANGE)?;
+    Ok(record)
 }
 
-pub fn get_client_change_cipher_spec_record(input: &[u8]) -> ChangeCipherSpecRecord {
-    let record = decode_change_cipher_spec_record(input)?;
+pub fn get_client_change_cipher_spec_record(
+    input: &[u8],
+) -> Result<ChangeCipherSpecRecord, DecoderError> {
+    let record = decode_change_cipher_spec_record(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::InvalidClientChangeCipherSpecRecord)?;
+
+    if &record.record_type != &TLS_RECORD_CHANGE_CIPHER_SPEC {
+        return Err(DecoderError::InvalidRecordType);
+    }
+    if &record.protocol_version != &TLS_PROTOCOL_VERSION {
+        return Err(DecoderError::UnsupportedProtocolVersion);
+    }
+    Ok(record)
 }
 
-pub fn get_client_handshake_finished_record(input: &[u8]) -> HandshakeFinishedRecord {
-    let record = decode_handshake_finished_record(input)?;
+pub fn get_client_handshake_finished_record(
+    input: &[u8],
+) -> Result<HandshakeFinishedRecord, DecoderError> {
+    let record = decode_handshake_finished_record(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::InvalidClientHandshakeFinishedRecord)?;
+
+    validate_handshake_header(&record.handshake_header, &TLS_HANDSHAKE_FINISHED)?;
+    Ok(record)
 }
 
-pub fn get_client_alert_record(input: &[u8]) -> ApplicationDataRecord {
-    let record = decode_application_data_record(input)?;
+pub fn get_client_alert_record(input: &[u8]) -> Result<ApplicationDataRecord, DecoderError> {
+    let record = decode_application_data_record(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::InvalidClientAlertRecord)?;
+
+    validate_record_header(&record.record_header, &TLS_RECORD_ALERT)?;
+    Ok(record)
 }
 
-pub fn get_client_application_data_record(input: &[u8]) -> ApplicationDataRecord {
-    let record = decode_application_data_record(input)?;
+pub fn get_client_application_data_record(
+    input: &[u8],
+) -> Result<ApplicationDataRecord, DecoderError> {
+    let record = decode_application_data_record(input)
+        .map(|(_, record)| record)
+        .map_err(DecoderError::InvalidClientApplicationDataRecord)?;
+
+    validate_record_header(&record.record_header, &TLS_RECORD_APPLICATION_DATA)?;
+    Ok(record)
 }
-
-
-// fn is_valid_client_hello_record(hello_record: HelloRecord) -> bool {
-//     if record_type != crate::tls::tls_handler::TLS_RECORD_HANDSHAKE {
-//         return Err(TlsHandlerError::InvalidRecord(record_type));
-//     }
-//
-//     if protocol_version != crate::tls::tls_handler::TLS_PROTOCOL_VERSION {
-//         return Err(TlsHandlerError::ProtocolNotSupported(crate::tls::tls_handler::TLS_PROTOCOL_VERSION));
-//     }
-//
-//     if handshake_type != crate::tls::tls_handler::TLS_HANDSHAKE_CLIENT_HELLO {
-//         return Err(TlsHandlerError::InvalidHandshake(handshake_type));
-//     }
-//
-//     if cipher_suite != crate::tls::tls_handler::TLS_RSA_AES_128_CBC_SHA_256 {
-//         return Err(TlsHandlerError::CipherNotSupported(cipher_suite));
-//     }
-//
-//     true
-// }
